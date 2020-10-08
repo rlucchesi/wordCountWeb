428c61f502b32db6e5011f381ea3bb53683f1482
Rafael Lucchesi <32688674+rlucchesi@users.noreply.github.com>
2020-10-07 21:22:22 -0700

    uploading my solution

---
+++ wordCountWeb.py
@@ -0,0 +1,30 @@
+# 
+# * Author:    Rafael Lucchesi
+# * Created:   2020/10/07
+#
+# Goal: count the number of words on a 'random' website
+#
+
+import urllib.request
+from bs4 import BeautifulSoup
+
+output = 0
+lineList = [ ]
+wordList = [ ]
+url = 'https://www.globalrelay.com/'
+
+htmlResponse = urllib.request.urlopen(url)
+htmlContent = htmlResponse.read()
+
+soup = BeautifulSoup(htmlContent, features="html.parser")
+
+lineList = soup.text.split('\n')
+
+for line in lineList:
+	if len(line) > 0:
+		wordList = line.split(' ')
+		for word in wordList:
+			if len(word) > 0:
+				output += 1
+
+print(output)
